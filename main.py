d950f29f475c05a16a8fbc398cfcdfa06756d54d
raideus138 <raivicuna@gmail.com>
2024-01-13 21:34:39 -0300

    un puro archivo q si funciona

--- main.py
+++ main.py
@@ -2,20 +2,44 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import pandas as pd
+
+emails = []
+total = 0
+def ece(archivo_excel, nombre_columna):
+    try:
+        df = pd.read_excel(archivo_excel)
+        columna = df[nombre_columna].tolist()
+        return columna
+    except Exception as e:
+        print(f"Error al leer el archivo Excel: {e}")
+        return None
 
 servidor_smtp = 'smtp.gmail.com'
 puerto_smtp = 587
 remitente = 'raivicuna@gmail.com'
 contraseña = 'foaa vysy lsgp zekj'
 
+archivo_excel = 'C:/Users/raivi/OneDrive/Desktop/4554/LIBROGMAILS.xlsx'
+nombre_columna = 'GMAILS'
+
+contenido_columna = ece(archivo_excel, nombre_columna)
+
+if contenido_columna:
+    print(f"Contenido de la columna '{nombre_columna}':")
+    for elemento in contenido_columna:
+        emails.append(elemento)
+else:
+    print("No se pudo extraer el contenido de la columna.")
+
 mensaje = MIMEMultipart()
 mensaje['From'] = remitente
-mensaje['Subject'] = 'correo automatico prueba 1\n 2 remitentes'
+mensaje['Subject'] = 'correo automático prueba 1, 2 remitentes'
 
 cuerpo = 'Contenido del correo'
 mensaje.attach(MIMEText(cuerpo, 'plain'))
 
-destinatarios = ['raivicuna@gmail.com', 'ivarquitecto@gmail.com']
+destinatarios = emails
 
 with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
     servidor.starttls()
@@ -25,5 +49,5 @@ with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
     for destinatario in destinatarios:
         servidor.sendmail(remitente, destinatario, mensaje.as_string())
         print(f"Correo enviado a {destinatario}")
-
-print("Todos los correos han sido enviados petee")
+        total=+1
+print(f"Todos los correos han sido enviados correctamente. Se enviaron {total} correos.")
